0d929a07cde5930333ea756c580f61a8c83d1667
Chris Tozer <christopherjtozer@gmail.com>
2016-03-19 21:02:04 +0000

    Missing file

---
+++ src/resources.h
@@ -0,0 +1,14 @@
+#ifndef RESOURCES_H_INCLUDED
+#define RESOURCES_H_INCLUDED
+
+struct ResourceCollection
+{
+    int food;
+    int grain;
+    int vegetables;
+    int sheep;
+    int boar;
+    int cattle;
+};
+
+#endif
